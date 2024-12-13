2cab5b686c90ccee48751882fcc59cb9484326fb
gerbeur <wesley.fage@ecole-hexagone.com>
2024-12-13 11:52:38 +0100

    exam push

---
+++ exam/name_prompt.asm
@@ -0,0 +1,45 @@
+section .data
+    prompt db 'Entre ton nom: ', 0
+    prompt_len equ $ - prompt
+    greeting db 'Bonjour, ', 0
+    greeting_len equ $ - greeting
+
+section .bss
+    name resb 256
+
+section .text
+    global _start
+
+_start:
+    ; Afficher le prompt
+    mov eax, 4         
+    mov ebx, 1          
+    mov ecx, prompt     
+    mov edx, prompt_len 
+    int 0x80            
+
+    ; Lire le nom de l'utilisateur
+    mov eax, 3          
+    mov ebx, 0          
+    mov ecx, name       
+    mov edx, 256        
+    int 0x80           
+
+    ; Afficher le message de salutation
+    mov eax, 4          
+    mov ebx, 1          
+    mov ecx, greeting   
+    mov edx, greeting_len 
+    int 0x80            
+
+    ; Afficher le nom de l'utilisateur
+    mov eax, 4         
+    mov ebx, 1          
+    mov ecx, name      
+    mov edx, 256        
+    int 0x80            
+
+    ; Terminer le programme
+    mov eax, 1          
+    xor ebx, ebx        
+    int 0x80            
